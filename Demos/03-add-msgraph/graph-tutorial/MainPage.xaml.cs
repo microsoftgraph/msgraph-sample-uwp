c161df95f513c859855bedfe5c468caa01fa5bc4
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-07-03 15:46:37 -0400

    Copied 02 demo to 03 folder

---
+++ Demos/03-add-msgraph/graph-tutorial/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.Toolkit.Services.MicrosoftGraph;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace graph_tutorial
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Initialize auth state to false
+            SetAuthState(false);
+
+            // Load OAuth settings
+            var oauthSettings = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView("OAuth");
+            var appId = oauthSettings.GetString("AppId");
+            var scopes = oauthSettings.GetString("Scopes");
+
+            if (string.IsNullOrEmpty(appId) || string.IsNullOrEmpty(scopes))
+            {
+                Notification.Show("Could not load OAuth Settings from resource file.");
+            }
+            else
+            {
+                // Initialize Graph
+                MicrosoftGraphService.Instance.AuthenticationModel = MicrosoftGraphEnums.AuthenticationModel.V2;
+                MicrosoftGraphService.Instance.Initialize(appId,
+                    MicrosoftGraphEnums.ServicesToInitialize.UserProfile,
+                    scopes.Split(' '));
+
+                // Navigate to HomePage.xaml
+                RootFrame.Navigate(typeof(HomePage));
+            }
+        }
+
+        private void SetAuthState(bool isAuthenticated)
+        {
+            (App.Current as App).IsAuthenticated = isAuthenticated;
+
+            // Toggle controls that require auth
+            Calendar.IsEnabled = isAuthenticated;
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var invokedItem = args.InvokedItem as string;
+
+            switch (invokedItem.ToLower())
+            {
+                case "calendar":
+                    throw new NotImplementedException();
+                    break;
+                case "home":
+                default:
+                    RootFrame.Navigate(typeof(HomePage));
+                    break;
+            }
+        }
+
+        private void Login_SignInCompleted(object sender, Microsoft.Toolkit.Uwp.UI.Controls.Graph.SignInEventArgs e)
+        {
+            // Set the auth state
+            SetAuthState(true);
+            // Reload the home page
+            RootFrame.Navigate(typeof(HomePage));
+        }
+
+        private void Login_SignOutCompleted(object sender, EventArgs e)
+        {
+            // Set the auth state
+            SetAuthState(false);
+            // Reload the home page
+            RootFrame.Navigate(typeof(HomePage));
+        }
+    }
+}
